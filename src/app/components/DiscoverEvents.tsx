d550cf6e8c0e50e811d43dbfb2e0f19d8342533c
M Waseem <muhammadwaseem2u@gmail.com>
2024-12-20 02:11:11 -0800

    bitbasel final design

--- src/app/components/DiscoverEvents.tsx
+++ src/app/components/DiscoverEvents.tsx
@@ -8,20 +8,19 @@ import image3 from "../../../public/event/event3.svg";
 const DiscoverEvents = () => {
   return (
     <div className="my-14 md:my-20 lg:my-[120px]">
-        <h3 className="font-space-grotesk font-bold text-base md:text-xl lg:text-3xl xl:text-[40px] text-black text-center uppercase py-14">
-          Other Products
-        </h3>
-        <div className=" flex justify-center items-center flex-wrap gap-5 md:gap-0 lg:justify-between  ">
-          {events.map((x, index) => (
-            <Image
-              key={index}
-              src={x.img1}
-              alt="events"
-              className="w-[339px] h-[264px]"
-            />
-          ))}
-        </div>
-      
+      <h3 className="font-space-grotesk font-bold text-base md:text-xl lg:text-3xl xl:text-[40px] text-black text-center uppercase py-14">
+        Other Products
+      </h3>
+      <div className=" flex justify-center items-center flex-wrap gap-5 md:gap-0 lg:justify-between  ">
+        {events.map((x, index) => (
+          <Image
+            key={index}
+            src={x.img1}
+            alt="events"
+            className="w-[339px] h-[264px]"
+          />
+        ))}
+      </div>
     </div>
   );
 };
